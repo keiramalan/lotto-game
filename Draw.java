ab808a02a6266dab9ae9a01472b5bc3b953e7c4d
keira malan <keira.malan@student.onslow.school.nz>
2021-03-11 12:15:29 +1300

    more dice

--- Draw.java
+++ Draw.java
@@ -18,9 +18,21 @@ public class Draw
         // create array
         int[] lottoDraw = new int[NUM_AMOUNT];
         // randomly assign number up to 40 in each section of array w for loop
+        for (int index = 0; index < NUM_AMOUNT; index ++) {
+            lottoDraw[index] = (int) ((Math.random() + 1) * LOTTO_LIMIT);
+            // new number
+            lottoDraw[index] = (int) ((Math.random() + 1) * LOTTO_LIMIT);
+            // if new number not in array aready add to array
+            // else loop for new number
+            // make it a while loop????
+        }
+            
         // loop through to check for repeats
         // nested loop
-        // check if x number is in for loop
+        //for (int checkFor: lottoDraw){
+            //for (int num: lottoDraw) {
+                //if (
+                // check if x number is in for loop
     }
     
     /*
@@ -30,3 +42,4 @@ public class Draw
         // return array
     }
 }
+
