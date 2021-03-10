453ed040adb5571f60c3c2fe6ec30d0971742566
keira malan <keira.malan@student.onslow.school.nz>
2021-03-11 12:04:03 +1300

    Initial sharing of project

---
+++ Draw.java
@@ -0,0 +1,32 @@
+
+/**
+ * Draw 6 numbers in an array for a lotto game
+ *
+ * @author Keira Malan
+ * @version 11/3/21
+ */
+public class Draw
+{
+    // constants - num of numbers, limit of lotto num (40 or sumn)
+    final int LOTTO_LIMIT = 40;
+    final int NUM_AMOUNT = 6;
+    
+    /*
+     * constructor for objects of class Draw
+     */
+    public Draw() {
+        // create array
+        int[] lottoDraw = new int[NUM_AMOUNT];
+        // randomly assign number up to 40 in each section of array w for loop
+        // loop through to check for repeats
+        // nested loop
+        // check if x number is in for loop
+    }
+    
+    /*
+     * Return final array
+     */
+    public int[] returnList() {
+        // return array
+    }
+}
